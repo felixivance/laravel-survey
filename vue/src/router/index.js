902e678a5e49d916eb5fce6d06beb6b6585ce84e
Felix <felixrunye@gmail.com>
2022-04-15 12:48:08 +0300

    index update

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -41,9 +41,11 @@ const router = createRouter( {
 
 router.beforeEach((to, from, next)=>{
     if(to.meta.requiresAuth && !store.state.user.token){
-      next({name:'Login'})
-    }else{
-      next()
+      next({name:'Login'});
+    }else if(store.state.user.token && (to.name === 'Login' || to.name === 'Register')){
+      next({name:'Dashboard'});
+     }else{
+      next();
     }
 });
 
