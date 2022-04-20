52ed7eabeca6a587c462c64e449561381dc332bf
Felix <felixrunye@gmail.com>
2022-04-20 12:51:55 +0300

    updated index sample data

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,6 +1,22 @@
 import { createStore } from "vuex";
 import axiosClient from "../axios";
 
+const tmpSurveys = [
+  {
+    id:100,
+    title:'Felix',
+    slug:'Felix is a G',
+    status:'draft',
+    image: "https://pbs.twimg.com/profile_images/1118059535003017221/9ZwEYqj2_400x400.png",
+    description:'My name is Felix, i am a web developer with 8+ years of exeperience',
+    created_at: "2021-12-20 18:00:00",
+    updated_at:"2021-12-20 18:00:00",
+    expire_date: "2021-12-31 18:00",
+    questions:[]
+
+  }
+];
+
 const store = createStore({
   state:{
     user:{
