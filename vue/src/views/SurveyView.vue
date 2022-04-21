5e82018fddc8b08b71ea70ce565bf10e75efa57a
Felix <felixrunye@gmail.com>
2022-04-21 11:07:33 +0300

    displaying the surveys on the web

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -4,7 +4,7 @@
     <template v-slot:header>
       <div class="flex justify-between items-center">
         <h1 class="text-3xl font-bold text-gray-900">View Surveys</h1>
-        <router-link :to="{name: 'CreateSurvey'}" class="py-2 px-3 text-white bg-emerald-500 rounded-md hover:bg-emerald-600">
+        <router-link :to="{name: 'Surveys'}" class="py-2 px-3 text-white bg-emerald-500 rounded-md hover:bg-emerald-600">
           <span class="flex space-x-2">
            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
