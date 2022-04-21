ae53e62962e733ad639a0dda64bdcbecf1375d6c
Felix <felixrunye@gmail.com>
2022-04-21 08:38:36 +0300

    page component update

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -171,7 +171,8 @@ const store = createStore({
     user:{
       data: {},
       token: sessionStorage.getItem("TOKEN")
-    }
+    },
+    surveys:[...tmpSurveys]
   },
   getters:{ },
   actions:{
