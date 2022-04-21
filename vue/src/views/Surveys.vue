7c0280f9d851b687586eed8a7b29a5a66e6ade7a
Felix <felixrunye@gmail.com>
2022-04-21 09:46:55 +0300

    show surveys on survey list dashboard

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -16,7 +16,11 @@
 
    </template>
     <div class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3">
-
+      <div v-for="survey in surveys" :key="survey.id" class="flex flex-col py-4 px-6 shadow-md bg-white hover:bg-gray-50 h-[470px]">
+        <img :src="survey.image" alt="img" class="w-full h-48 object-cover" />
+        <h4 class="mt-4 text-lg font-bold">{{survey.title}}</h4>
+        <div v-html="survey.description" class="overflow-hidden flex-1"></div>
+      </div>
     </div>
   </PageComponent>
 </template>
