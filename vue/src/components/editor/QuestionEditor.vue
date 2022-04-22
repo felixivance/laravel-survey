18074e9b0c5496189b303a26e934061635573d84
Felix <felixrunye@gmail.com>
2022-04-22 08:28:38 +0300

    reformating question editor

--- vue/src/components/editor/QuestionEditor.vue
+++ vue/src/components/editor/QuestionEditor.vue
@@ -8,16 +8,11 @@
 
     <div class="flex items-center">
       <!-- Add new question -->
-      <button type="button" @click="addQuestion()"
-        class=" flex items-center text-xs py-1 px-3 mr-2 rounded-sm text-white bg-gray-600 hover:bg-gray-700">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-            clip-rule="evenodd"
-          />
+      <button class=" flex items-center text-xs py-1 px-3 mr-2 rounded-sm text-white bg-gray-600 hover:bg-gray-700" type="button"
+              @click="addQuestion()">
+        <svg class="h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" >
+          <path clip-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+            fill-rule="evenodd"/>
         </svg>
         Add
       </button>
@@ -25,31 +20,11 @@
 
       <!-- Delete question -->
       <button
-        type="button"
-        @click="deleteQuestion()"
-        class="
-          flex
-          items-center
-          text-xs
-          py-1
-          px-3
-          rounded-sm
-          border border-transparent
-          text-red-500
-          hover:border-red-600
-        "
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-4 w-4"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-            clip-rule="evenodd"
-          />
+        class=" flex items-center text-xs py-1 px-3 rounded-sm border border-transparent text-red-500  hover:border-red-600"
+        type="button" @click="deleteQuestion()" >
+        <svg class="h-4 w-4"  fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path clip-rule="evenodd"  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+            fill-rule="evenodd"/>
         </svg>
         Delete
       </button>
@@ -60,17 +35,12 @@
   <div class="grid gap-3 grid-cols-12">
     <!-- Question -->
     <div class="mt-3 col-span-9">
-      <label
-        :for="'question_text_' + model.data"
-        class="block text-sm font-medium text-gray-700"
-      >Question Text</label
+      <label :for="'question_text_' + model.data" class="block text-sm font-medium text-gray-700">Question Text</label
       >
       <input
-        type="text"
-        :name="'question_text_' + model.data"
-        v-model="model.question"
-        @change="dataChange"
         :id="'question_text_' + model.data"
+        v-model="model.question"
+        :name="'question_text_' + model.data"
         class="
           mt-1
           focus:ring-indigo-500 focus:border-indigo-500
@@ -81,20 +51,20 @@
           border-gray-300
           rounded-md
         "
+        type="text"
+        @change="dataChange"
       />
     </div>
     <!--/ Question -->
 
     <!-- Question Type -->
     <div class="mt-3 col-span-3">
-      <label for="question_type" class="block text-sm font-medium text-gray-700"
+      <label class="block text-sm font-medium text-gray-700" for="question_type"
       >Select Question Type</label
       >
       <select
         id="question_type"
-        name="question_type"
         v-model="model.type"
-        @change="typeChange"
         class="
           mt-1
           block
@@ -108,6 +78,8 @@
           focus:outline-none focus:ring-indigo-500 focus:border-indigo-500
           sm:text-sm
         "
+        name="question_type"
+        @change="typeChange"
       >
         <option v-for="type in questionTypes" :key="type" :value="type">
           {{ upperCaseFirst(type) }}
@@ -125,10 +97,9 @@
     >Description</label
     >
     <textarea
-      :name="'question_description_' + model.id"
-      v-model="model.description"
-      @change="dataChange"
       :id="'question_description_' + model.id"
+      v-model="model.description"
+      :name="'question_description_' + model.id"
       class="
         mt-1
         focus:ring-indigo-500 focus:border-indigo-500
@@ -139,6 +110,7 @@
         border-gray-300
         rounded-md
       "
+      @change="dataChange"
     />
   </div>
   <!--/ Question Description -->
@@ -151,8 +123,6 @@
 
         <!-- Add new option -->
         <button
-          type="button"
-          @click="addOption()"
           class="
             flex
             items-center
@@ -164,17 +134,19 @@
             bg-gray-600
             hover:bg-gray-700
           "
+          type="button"
+          @click="addOption()"
         >
           <svg
-            xmlns="http://www.w3.org/2000/svg"
             class="h-4 w-4"
-            viewBox="0 0 20 20"
             fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
-              d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
               clip-rule="evenodd"
+              d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+              fill-rule="evenodd"
             />
           </svg>
           Add Option
@@ -196,10 +168,7 @@
       >
         <span class="w-6 text-sm"> {{ index + 1 }}. </span>
         <input
-          type="text"
-          tabindex="1"
           v-model="option.text"
-          @change="dataChange"
           class="
             w-full
             rounded-sm
@@ -209,11 +178,12 @@
             border border-gray-300
             focus:border-indigo-500
           "
+          tabindex="1"
+          type="text"
+          @change="dataChange"
         />
         <!-- Delete Option -->
         <button
-          type="button"
-          @click="removeOption(option)"
           class="
             h-6
             w-6
@@ -225,17 +195,19 @@
             transition-colors
             hover:border-red-100
           "
+          type="button"
+          @click="removeOption(option)"
         >
           <svg
-            xmlns="http://www.w3.org/2000/svg"
             class="h-3 w-3 text-red-500"
-            viewBox="0 0 20 20"
             fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
-              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
               clip-rule="evenodd"
+              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+              fill-rule="evenodd"
             />
           </svg>
         </button>
@@ -246,13 +218,14 @@
   </div>
   <!--/ Data -->
 
-  <hr class="my-4" />
+  <hr class="my-4"/>
 </template>
 
 <script setup>
-import { v4 as uuidv4 } from "uuid";
-import { computed, ref } from "@vue/reactivity";
+import {v4 as uuidv4} from "uuid";
+import {computed, ref} from "@vue/reactivity";
 import store from "../../store";
+
 const props = defineProps({
   question: Object,
   index: Number,
@@ -262,38 +235,46 @@ const emit = defineEmits(["change", "addQuestion", "deleteQuestion"]);
 const model = ref(JSON.parse(JSON.stringify(props.question)));
 // Get question types from vuex
 const questionTypes = computed(() => store.state.questionTypes);
+
 function upperCaseFirst(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
 function getOptions() {
   return model.value.data.options;
 }
+
 function setOptions(options) {
   model.value.data.options = options;
 }
+
 // Check if the question should have options
 function shouldHaveOptions() {
   return ["select", "radio", "checkbox"].includes(model.value.type);
 }
+
 // Add option
 function addOption() {
   setOptions([
     ...getOptions(),
-    { uuid: uuidv4(), text: "" },
+    {uuid: uuidv4(), text: ""},
   ]);
   dataChange();
 }
+
 // Remove option
 function removeOption(op) {
   setOptions(getOptions().filter((opt) => opt !== op));
   dataChange();
 }
+
 function typeChange() {
   if (shouldHaveOptions()) {
     setOptions(getOptions() || []);
   }
   dataChange();
 }
+
 // Emit the data change
 function dataChange() {
   const data = model.value;
@@ -302,9 +283,11 @@ function dataChange() {
   }
   emit("change", data);
 }
+
 function addQuestion() {
   emit("addQuestion", props.index + 1);
 }
+
 function deleteQuestion() {
   emit("deleteQuestion", props.question);
 }
