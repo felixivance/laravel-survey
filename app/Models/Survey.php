a2d84908a871688379a35a68902e9cb8e651c769
Felix <felixrunye@gmail.com>
2022-04-26 20:49:59 +0300

    added fillables to survey

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Survey extends Model
 {
     use HasFactory;
+    protected $fillable=['user_id','title','slug','status','description','expire_date'];
+
 }
