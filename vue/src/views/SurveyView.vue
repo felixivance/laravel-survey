f83b7b73638721f090eeb416ef97034d62ef875e
Felix <felixrunye@gmail.com>
2022-05-12 21:48:23 +0300

    delete survey functionality

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -3,15 +3,23 @@
   <PageComponent >
     <template v-slot:header>
       <div class="flex justify-between items-center">
-        <h1 class="text-3xl font-bold text-gray-900">{{ route.params.id ? "Editing: "+survey.title : "Create Survey"  }}</h1>
-        <router-link :to="{name: 'Surveys'}" class="py-2 px-3 text-white bg-emerald-500 rounded-md hover:bg-emerald-600">
-          <span class="flex space-x-2">
-           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-          </svg>
-         Back to List
-          </span>
-        </router-link>
+        <div>
+          <h1 class="text-3xl font-bold text-gray-900">{{ route.params.id ? "Editing: "+survey.title : "Create Survey"  }}</h1>
+        </div>
+        <div class="flex items-center space-x-2">
+          <router-link :to="{name: 'Surveys'}" class="py-2 px-3 text-white bg-emerald-500 rounded-md hover:bg-emerald-600">
+            <span class="flex space-x-2">
+             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+            </svg>
+           Back to List
+            </span>
+          </router-link>
+          <button v-if="route.params.id" type="button" class="py-2 px-3 text-white bg-red-500 hover:bg-red-600 rounded-md"
+          @click="deleteSurvey">
+            Delete
+          </button>
+        </div>
       </div>
 
     </template>
