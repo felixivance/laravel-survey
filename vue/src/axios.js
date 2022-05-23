00154c280d1a2484ec55db090644e21cfa3ac866
Felix <felixrunye@gmail.com>
2022-05-23 21:33:01 +0300

    updated survey controller

--- vue/src/axios.js
+++ vue/src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from "./store";
 
 const axiosClient = axios.create({
-  baseURL:'http://127.0.0.1:8001/api'
+  baseURL:'http://127.0.0.1:8001/api' //todo change here when going live
 })
 
 axiosClient.interceptors.request.use(config=>{
