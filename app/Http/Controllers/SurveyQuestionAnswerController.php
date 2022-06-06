fde34d252cfc500cd8588ac1077e97f489644729
Felix <felixrunye@gmail.com>
2022-06-06 17:04:41 +0300

    removed boiler plate code

--- app/Http/Controllers/SurveyQuestionAnswerController.php
+++ app/Http/Controllers/SurveyQuestionAnswerController.php
@@ -27,20 +27,6 @@ class SurveyQuestionAnswerController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-
-
-
 
 
 
