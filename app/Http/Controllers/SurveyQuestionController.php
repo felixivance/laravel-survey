bac622101b2175cab0d2d98befae458dcbe9fe6c
Felix <felixrunye@gmail.com>
2022-06-06 17:05:29 +0300

    removed boiler plate code

--- app/Http/Controllers/SurveyQuestionController.php
+++ app/Http/Controllers/SurveyQuestionController.php
@@ -12,14 +12,4 @@ class SurveyQuestionController extends Controller
 
 
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\SurveyQuestion  $surveyQuestion
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(SurveyQuestion $surveyQuestion)
-    {
-        //
-    }
 }
