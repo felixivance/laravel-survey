21879b94e1eaea33daea47534547166d5cd82fdc
Felix <felixrunye@gmail.com>
2022-06-06 17:05:08 +0300

    removed boiler plate code

--- app/Http/Controllers/SurveyQuestionController.php
+++ app/Http/Controllers/SurveyQuestionController.php
@@ -9,16 +9,6 @@ class SurveyQuestionController extends Controller
 {
 
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
