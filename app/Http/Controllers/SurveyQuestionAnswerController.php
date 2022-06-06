c14e90206cc255301feda7010ac397c30cb3fc06
Felix <felixrunye@gmail.com>
2022-06-06 17:04:51 +0300

    removed boiler plate code

--- app/Http/Controllers/SurveyQuestionAnswerController.php
+++ app/Http/Controllers/SurveyQuestionAnswerController.php
@@ -7,16 +7,6 @@ use Illuminate\Http\Request;
 
 class SurveyQuestionAnswerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
 
 
 }
