5ad12182a9c4a2eecfd72dfd0a5f0d452b90b4e4
Felix <felixrunye@gmail.com>
2022-06-07 17:20:06 +0300

    code cleanup

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -76,10 +76,8 @@
             </div>
           </div>
           <div class="text-center text-gray-600"  v-if="!survey.questions.length">
-<!--             -->
             You dont have any questions created
           </div>
-<!--          {{survey.questions}}-->
           <div v-for="(question, index) in survey.questions" :key="question.id">
             <QuestionEditor  :question="question" :index="index" @change="questionChange" @addQuestion="addQuestion" @deleteQuestion="deleteQuestion"  />
           </div>
