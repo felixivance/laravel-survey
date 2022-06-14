2815dc6e0669db6fd0a247b3eb23bc2842fa236c
Felix <felixrunye@gmail.com>
2022-06-14 21:33:36 +0300

    updated router js

--- resources/js/router.js
+++ resources/js/router.js
@@ -5,6 +5,10 @@ Vue.use(VueRouter);
 
 import dashboardComponent from './components/dashboardComponent.vue'
 import loginComponent from './components/loginComponent.vue'
+import DefaultLayout from "../../vue/src/components/DefaultLayout";
+import Dashboard from "../../vue/src/views/Dashboard";
+import Surveys from "../../vue/src/views/Surveys";
+import SurveyView from "../../vue/src/views/SurveyView";
 
 export default new VueRouter({
   mode: 'history',
@@ -18,9 +22,25 @@ export default new VueRouter({
       component: loginComponent
     },
     {
-      path: '/main/dashboard',
-      component: dashboardComponent,
-      name: 'dashboard'
+      path:'/',
+      name: 'Dashboard',
+      redirect: '/dashboard',
+      component: DefaultLayout,
+      meta: {requiresAuth:true},
+      children: [
+        {
+          path:'/dashboard', name:'Dashboard', component: Dashboard
+        },
+        // {
+        //   path:'/surveys', name:'Surveys', component: Surveys
+        // },
+        // {
+        //   path:'/surveys/create', name:'CreateSurvey', component: SurveyView
+        // },
+        // {
+        //   path:'/surveys/:id', name:'UpdateSurvey', component: SurveyView
+        // }
+      ]
     },
   ],
 });
