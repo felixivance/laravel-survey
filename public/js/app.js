8e5d9d1cd91893ed318e8bc04008f0e2818919f0
Felix <felixrunye@gmail.com>
2022-06-14 21:49:38 +0300

    updated commit to latest commit

--- public/js/app.js
+++ public/js/app.js
@@ -5498,7 +5498,7 @@ __webpack_require__.r(__webpack_exports__);
     }
   },
   mounted: function mounted() {
-    console.log;
+    console.log(this.$route);
   }
 });
 
--- resources/js/components/DefaultLayout.vue
+++ resources/js/components/DefaultLayout.vue
@@ -172,7 +172,7 @@ export default{
     },
   },
   mounted() {
-    console.log
+    console.log(this.$route)
   }
 }
 </script>
