e848ac587f29510734bb3625ddeedd6f01075988
Felix <felixrunye@gmail.com>
2022-06-19 22:12:08 +0300

    updated survey editing

--- public/js/app.js
+++ public/js/app.js
@@ -5607,8 +5607,8 @@ __webpack_require__.r(__webpack_exports__);
     survey: Object
   },
   methods: {
-    "delete": function _delete(survey) {
-      this.emit('delete', survey); // const emit = defineEmits(['delete']);
+    deleteSurvey: function deleteSurvey(survey) {
+      this.$emit('delete', survey); // const emit = defineEmits(['delete']);
     }
   }
 });
@@ -31061,8 +31061,119 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "render": () => (/* binding */ render),
 /* harmony export */   "staticRenderFns": () => (/* binding */ staticRenderFns)
 /* harmony export */ });
-var render = function () {}
+var render = function () {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c(
+    "div",
+    {
+      staticClass:
+        "flex flex-col py-4 px-6 shadow-md bg-white hover:bg-gray-50 h-[470px]",
+    },
+    [
+      _c("img", {
+        staticClass: "w-full h-48 object-cover",
+        attrs: { src: _vm.survey.image_url, alt: "img" },
+      }),
+      _vm._v(" "),
+      _c("h4", { staticClass: "mt-4 text-lg font-bold" }, [
+        _vm._v(_vm._s(_vm.survey.title)),
+      ]),
+      _vm._v(" "),
+      _c("div", {
+        staticClass: "overflow-hidden flex-1",
+        domProps: { innerHTML: _vm._s(_vm.survey.description) },
+      }),
+      _vm._v(" "),
+      _c(
+        "div",
+        { staticClass: "flex justify-between items-center mt-3" },
+        [
+          _c(
+            "router-link",
+            {
+              staticClass:
+                "flex items-center space-x-1 py-2 px-4 border border-transparent text-sm rounded-md text-white bg-indigo-600 hover:bg-indigo-700\n                focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500",
+              attrs: {
+                to: { name: "UpdateSurvey", params: { id: _vm.survey.id } },
+              },
+            },
+            [
+              _c(
+                "svg",
+                {
+                  staticClass: "h-4 w-4",
+                  attrs: {
+                    xmlns: "http://www.w3.org/2000/svg",
+                    fill: "none",
+                    viewBox: "0 0 24 24",
+                    stroke: "currentColor",
+                    "stroke-width": "2",
+                  },
+                },
+                [
+                  _c("path", {
+                    attrs: {
+                      "stroke-linecap": "round",
+                      "stroke-linejoin": "round",
+                      d: "M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z",
+                    },
+                  }),
+                ]
+              ),
+              _vm._v(" "),
+              _c("p", [_vm._v("Edit")]),
+            ]
+          ),
+          _vm._v(" "),
+          _vm.survey.id
+            ? _c(
+                "button",
+                {
+                  staticClass:
+                    "h-8 w-8 flex items-center justify-center rounded-full\n              border border-transparent text-sm text-red-500 focus:ring-2 focus:ring-offset-2 focus:ring-red-500",
+                  attrs: { type: "button" },
+                  on: {
+                    click: function ($event) {
+                      return _vm.deleteSurvey(_vm.survey)
+                    },
+                  },
+                },
+                [
+                  _c(
+                    "svg",
+                    {
+                      staticClass: "h-6 w-6 text-",
+                      attrs: {
+                        xmlns: "http://www.w3.org/2000/svg",
+                        fill: "none",
+                        viewBox: "0 0 24 24",
+                        stroke: "currentColor",
+                        "stroke-width": "2",
+                      },
+                    },
+                    [
+                      _c("path", {
+                        attrs: {
+                          "stroke-linecap": "round",
+                          "stroke-linejoin": "round",
+                          d: "M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16",
+                        },
+                      }),
+                    ]
+                  ),
+                ]
+              )
+            : _vm._e(),
+        ],
+        1
+      ),
+    ]
+  )
+}
 var staticRenderFns = []
+render._withStripped = true
 
 
 
