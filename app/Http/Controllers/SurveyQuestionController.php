1e04adde0c8cb3c58294e084c40471c19bcb10b1
Felix <felixrunye@gmail.com>
2022-07-05 22:29:10 +0300

    survey questions controller update

--- app/Http/Controllers/SurveyQuestionController.php
+++ app/Http/Controllers/SurveyQuestionController.php
@@ -16,13 +16,13 @@ class SurveyQuestionController extends Controller
             'survey_id' => 'required|integer',
             'description' => 'nullable|string',
         ]);
-        $surveyQuestion = SurveyQuestion::create($request->all());
+        $surveyQuestion = SurveyQuestion::query()->create($request->all());
         return response()->json($surveyQuestion, 201);
     }
 
     public function show($id)
     {
-        $surveyQuestion = SurveyQuestion::find($id);
+        $surveyQuestion = SurveyQuestion::query()->find($id);
         return response()->json($surveyQuestion, 200);
     }
 
@@ -35,14 +35,14 @@ class SurveyQuestionController extends Controller
             'survey_id' => 'required|integer',
             'description' => 'nullable|string',
         ]);
-        $surveyQuestion = SurveyQuestion::find($id);
+        $surveyQuestion = SurveyQuestion::query()->find($id);
         $surveyQuestion->update($request->all());
         return response()->json($surveyQuestion, 200);
     }
 
     public function delete($id)
     {
-        $surveyQuestion = SurveyQuestion::find($id);
+        $surveyQuestion = SurveyQuestion::query()->find($id);
         $surveyQuestion->delete();
         return response()->json(null, 204);
     }
