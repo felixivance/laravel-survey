79f7e3d9955aa845b8f30e082b9b79085826ea8d
Felix <felixrunye@gmail.com>
2022-07-05 22:27:22 +0300

    survey questions controller update

--- app/Http/Controllers/SurveyQuestionController.php
+++ app/Http/Controllers/SurveyQuestionController.php
@@ -7,5 +7,43 @@ use Illuminate\Http\Request;
 
 class SurveyQuestionController extends Controller
 {
+    public function store(Request $request)
+    {
+        $request->validate([
+            'question' => 'required|string',
+            'data' => 'required|string',
+            'type' => 'required|string',
+            'survey_id' => 'required|integer',
+            'description' => 'nullable|string',
+        ]);
+        $surveyQuestion = SurveyQuestion::create($request->all());
+        return response()->json($surveyQuestion, 201);
+    }
 
+    public function show($id)
+    {
+        $surveyQuestion = SurveyQuestion::find($id);
+        return response()->json($surveyQuestion, 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'question' => 'required|string',
+            'data' => 'required|string',
+            'type' => 'required|string',
+            'survey_id' => 'required|integer',
+            'description' => 'nullable|string',
+        ]);
+        $surveyQuestion = SurveyQuestion::find($id);
+        $surveyQuestion->update($request->all());
+        return response()->json($surveyQuestion, 200);
+    }
+
+    public function delete($id)
+    {
+        $surveyQuestion = SurveyQuestion::find($id);
+        $surveyQuestion->delete();
+        return response()->json(null, 204);
+    }
 }
