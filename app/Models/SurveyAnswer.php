32f23479428a44a7138b192ae2ff6c3d35315c94
Felix <felixrunye@gmail.com>
2022-07-05 22:20:29 +0300

    model relationship

--- app/Models/SurveyAnswer.php
+++ app/Models/SurveyAnswer.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class SurveyAnswer extends Model
 {
     use HasFactory;
+
+    protected $fillable=['user_id','survey_id','question_id','answer'];
+
+    public function user(){
+        return $this->belongsTo(User::class,'user_id');
+    }
+
+    public function survey(){
+        return $this->belongsTo(Survey::class,'survey_id');
+    }
 }
