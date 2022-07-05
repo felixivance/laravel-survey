06c58e24069b81dd2e99bb8471d88861d87d0e42
Felix <felixrunye@gmail.com>
2022-07-05 22:23:01 +0300

    model relationship

--- app/Http/Controllers/SurveyAnswerController.php
+++ app/Http/Controllers/SurveyAnswerController.php
@@ -9,15 +9,7 @@ class SurveyAnswerController extends Controller
 {
 
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+
 
     /**
      * Store a newly created resource in storage.
