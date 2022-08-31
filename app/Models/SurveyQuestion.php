59794133cd02904fd655742e8071d3a4b99e8bd5
Felix <felixrunye@gmail.com>
2022-08-31 23:31:34 +0300

    model update

--- app/Models/SurveyQuestion.php
+++ app/Models/SurveyQuestion.php
@@ -4,14 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SurveyQuestion extends Model
 {
     use HasFactory;
 
-    protected $fillable=['question','data','type','survey_id','description'];
+    protected $fillable = ['question', 'data', 'type', 'survey_id', 'description'];
 
-    public function survey(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    public function survey(): BelongsTo
     {
         return $this->belongsTo(Survey::class, 'survey_id');
     }
