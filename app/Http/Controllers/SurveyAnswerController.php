50f2d2a360a9340422ea9be31ada1bdd72109719
Felix <felixrunye@gmail.com>
2022-08-31 23:29:52 +0300

    survey answer update

--- app/Http/Controllers/SurveyAnswerController.php
+++ app/Http/Controllers/SurveyAnswerController.php
@@ -7,23 +7,23 @@ use Illuminate\Http\Request;
 
 class SurveyAnswerController extends Controller
 {
-        public function store(Request $request)
-        {
-            $request->validate([
-                'user_id' => 'required|integer',
-                'survey_id' => 'required|integer',
-                'question_id' => 'required|integer',
-                'answer' => 'required|string',
-            ]);
-            $surveyAnswer = SurveyAnswer::create($request->all());
-            return response()->json($surveyAnswer, 201);
-        }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'user_id' => 'required|integer',
+            'survey_id' => 'required|integer',
+            'question_id' => 'required|integer',
+            'answer' => 'required|string',
+        ]);
+        $surveyAnswer = SurveyAnswer::create($request->all());
+        return response()->json($surveyAnswer, 201);
+    }
 
-        public function show($id)
-        {
-            $surveyAnswer = SurveyAnswer::find($id);
-            return response()->json($surveyAnswer, 200);
-        }
+    public function show($id)
+    {
+        $surveyAnswer = SurveyAnswer::find($id);
+        return response()->json($surveyAnswer, 200);
+    }
 
 
 }
