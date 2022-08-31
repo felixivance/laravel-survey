147c27edb236f5ddc97342006c2fc64658912564
Felix <felixrunye@gmail.com>
2022-08-31 23:22:37 +0300

    auth controller update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,61 +10,64 @@ use Illuminate\Validation\Rules\Password;
 
 class AuthController extends Controller
 {
-    public function register(Request $request){
+    public function register(Request $request)
+    {
 
 //        return bcrypt('password');
 
         $data = $request->validate([
-            'name'=> 'required|string',
-            'email'=> 'required|email|string|unique:users',
-            'password'=> ['required','confirmed',
+            'name' => 'required|string',
+            'email' => 'required|email|string|unique:users',
+            'password' => ['required', 'confirmed',
                 Password::min(8)->mixedCase()->numbers()->symbols()
             ]
         ]);
         $user = User::create([
-            'name'=> $data['name'],
-            'email'=> $data['email'],
-            'password'=> bcrypt($data['password'])
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
         ]);
 
         $token = $user->createToken('main')->plainTextToken;
 
         return response([
-            'user'=>$user,
-            'token'=>$token,
-            'password'=>$data['password']
+            'user' => $user,
+            'token' => $token,
+            'password' => $data['password']
         ]);
     }
 
-    public function login(Request $request){
+    public function login(Request $request)
+    {
 
         $data = $request->validate([
-            'email'=>'required|string|exists:users,email',
-            'password'=>'required',
-            'remember_token'=>'boolean'
+            'email' => 'required|string|exists:users,email',
+            'password' => 'required',
+            'remember_token' => 'boolean'
         ]);
         $remember = $data['remember_token'] ?? false;
 
-        if(!Auth::attempt($data, $remember)){
+        if (!Auth::attempt($data, $remember)) {
             return response([
-                'error'=>'The provided credentials are not correct'
+                'error' => 'The provided credentials are not correct'
             ], 200);
         }
 
         $user = Auth::user();
         $token = $user->createToken('main')->plainTextToken;
         return response([
-            'user'=>$user,
-            'token'=>$token
+            'user' => $user,
+            'token' => $token
         ]);
     }
 
-    public function logout(){
+    public function logout()
+    {
         $user = Auth::user();
         $user->currentAccessToken()->delete();
 
         return response([
-            'success'=>true
+            'success' => true
         ]);
     }
 }
