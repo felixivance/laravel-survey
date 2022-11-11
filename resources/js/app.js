9c3dc120ba106d24fdaa9f689e2b0bd865154e82
Felix <felixrunye@gmail.com>
2022-11-11 21:20:36 +0300

    axios update

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,13 +15,6 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 import store from './store/index';
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
